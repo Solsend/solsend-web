30b2226cdd6293044b1ea95cb1dca035ea68eb9b
yummyweb <antriksh.timepass@gmail.com>
2023-03-13 20:27:37 +0530

    updated links with docs

--- pages/index.js
+++ pages/index.js
@@ -146,7 +146,7 @@ export default function Home() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="https://docs.page/solsend/solsend-web/getting-started/"
+                href="https://docs.solsend.dev/getting-started"
                 className="rounded-md bg-violet-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started

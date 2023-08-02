fec3da9e3164d36acb02e0b3df9ad7fb3d9506cc
yummyweb <antriksh.timepass@gmail.com>
2023-08-02 16:54:10 +0530

    updated team section

--- components/Teams.jsx
+++ components/Teams.jsx
@@ -7,7 +7,7 @@ const people = [
     },
     {
         name: 'Antara Verma',
-        role: 'Director of Marketing',
+        role: 'Co-Founder',
         imageUrl:
             'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
